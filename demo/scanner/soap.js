8bf6aad3c036b9838f4b6b0aa7a4d014917d1ea9
rockdragon <moyerock@gmail.com>
2015-10-26 19:41:05 +0800

    SOAP coarse ver: DONE.

--- demo/scanner/soap.js
+++ demo/scanner/soap.js
@@ -1,26 +1,26 @@
 var request = require("request");
 var to_json = require("xmljson").to_json;
+//var chunked_streams = require('chunking-streams');
 
 var _SOAP_HEADER =
-    '<?xml version="1.0" encoding="UTF-8"?>\n'+
+    '<?xml version="1.0" encoding="UTF-8"?>\n' +
     '<SOAP-ENV:Envelope \n' +
-        'xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" \n'+
-        'xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" \n'+
-        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \n'+
-        'xmlns:xsd="http://www.w3.org/2001/XMLSchema" \n'+
-        'xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope" \n'+
-        'xmlns:SOAP-ENC="http://www.w3.org/2003/05/soap-encoding" \n'+
-        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \n'+
-        'xmlns:xsd="http://www.w3.org/2001/XMLSchema" \n'+
-        'xmlns:wscn="http://tempuri.org/wscn.xsd" \n'+
-        'xmlns:wsdl="http://tempuri.org/wsdl.xsd">'+
+    'xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" \n' +
+    'xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" \n' +
+    'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \n' +
+    'xmlns:xsd="http://www.w3.org/2001/XMLSchema" \n' +
+    'xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope" \n' +
+    'xmlns:SOAP-ENC="http://www.w3.org/2003/05/soap-encoding" \n' +
+    'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \n' +
+    'xmlns:xsd="http://www.w3.org/2001/XMLSchema" \n' +
+    'xmlns:wscn="http://tempuri.org/wscn.xsd" \n' +
+    'xmlns:wsdl="http://tempuri.org/wsdl.xsd">\n' +
     '<SOAP-ENV:Body SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">\n';
 var _SOAP_TAIL = '</SOAP-ENV:Body></SOAP-ENV:Envelope>';
 
-_wrap_wscn_func = function _wrap_wscn_func(uri, xml){
+function _wrap_wscn_immediate_func(uri, xml) {
     var req_xml = _SOAP_HEADER + xml + _SOAP_TAIL;
-    console.log(req_xml)
-    return function(cb) {
+    return function (cb) {
         request({
             uri: uri,
             method: "POST",
@@ -31,77 +31,133 @@ _wrap_wscn_func = function _wrap_wscn_func(uri, xml){
                 "Content-Length": req_xml.length,
                 "SOAPAction": ""
             },
-        }, function(err, message, response){
-            if(err) return cb(err);
-            to_json(response, function(err, json){
-                if(err) return cb(err);
+        }, function (err, message, response) {
+            if (err) return cb(err);
+            to_json(response, function (err, json) {
+                if (err) return cb(err);
 
                 return cb(undefined, json && json["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]);
             });
         });
     };
-}
+};
 
-function ScannerSoap(uri){
+function _wrap_wscn_streaming_func(uri, xml, write_stream) {
+    var req_xml = _SOAP_HEADER + xml + _SOAP_TAIL;
+    return function (cb) {
+        request({
+            uri: uri,
+            method: "POST",
+            body: req_xml,
+            headers: {
+                "Content-Type": "text/xml; charset=utf-8",
+                "User-Agent": "gSOAP/2.7",
+                "Content-Length": req_xml.length,
+                "SOAPAction": ""
+            },
+        }, function (err, message, response) {
+            if (err) return cb(err);
+            to_json(response, function (err, json) {
+                if (err) return cb(err);
+
+                return cb(undefined, json && json["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]);
+            });
+        })
+        .pipe(write_stream);
+    };
+};
+
+function ScannerSoap(uri) {
     this.uri = uri
 }
 
-ScannerSoap.prototype.GetScannerElements = function GetScannerElements(cb) {
+function _get_scanner_elements(uri, cb) {
     var xml = '<wscn:GetScannerElements></wscn:GetScannerElements>';
-    var func = _wrap_wscn_func(this.uri, xml);
+    var func = _wrap_wscn_immediate_func(uri, xml);
     return func(cb);
 };
 
-ScannerSoap.prototype.CreateScanJobRequest = function CreateScanJobRequest(cb) {
+function _create_scan_job_request(uri, cb) {
     var xml =
-        '<wscn:CreateScanJobRequest>' +
-            '<JobIdentifier>Scan Identifier</JobIdentifier>'+
-            '<DestinationToken>Scan Dest Token</DestinationToken>'+
-            '<ScanTicket>'+
-                '<JobDescription>'+
-                    '<JobName>Scan job1</JobName>'+
-                    '<JobOriginationUserName>Scan user1</JobOriginationUserName>'+
-                    '<JobInformation>Scan job info1</JobInformation>'+
-                '</JobDescription>'+
-                '<DocumentParameters>'+
-                    '<Format>jfif</Format>'+
-                    '<CompressionQualityFactor>80</CompressionQualityFactor>'+
-                    '<ImagesToTransfer>0</ImagesToTransfer>'+
-                    '<InputSource>Platen</InputSource>'+
-                    '<ContentType>Auto</ContentType>'+
-                    '<InputSize>'+
-                        '<InputMediaSize>'+
-                            '<Width Override="true">8500</Width>'+
-                            '<Height Override="true">11690</Height>'+
-                        '</InputMediaSize>'+
-                        '<DocumentSizeAutoDetect>false</DocumentSizeAutoDetect>'+
-                    '</InputSize>'+
-                    '<Exposure>'+
-                        '<AutoExposure>true</AutoExposure>'+
-                        '<ExposureSettings>true</ExposureSettings>'+
-                    '</Exposure>'+
-                    '<MediaSides>'+
-                        '<MediaFront>' +
-                            '<ScanRegion>'+
-                                '<ScanRegionXOffset UsedDefault="true">0</ScanRegionXOffset>'+
-                                '<ScanRegionYOffset UsedDefault="true">0</ScanRegionYOffset>'+
-                                '<ScanRegionWidth UsedDefault="true">8267</ScanRegionWidth>'+
-                                '<ScanRegionHeight UsedDefault="true">11690</ScanRegionHeight>'+
-                            '</ScanRegion>'+
-                            '<ColorProcessing UsedDefault="true">RGB24</ColorProcessing>'+
-                            '<Resolution>'+
-                                '<Width>300</Width>'+
-                                '<Height>300</Height>'+
-                            '</Resolution>'+
-                        '</MediaFront>'+
-                    '</MediaSides>'+
-                '</DocumentParameters>'+
-                '<RetrieveImageTimeout>0</RetrieveImageTimeout>'+
-                '<ScanManufacturingParameters><DisableImageProcessing>false</DisableImageProcessing></ScanManufacturingParameters>'+
-            '</ScanTicket>'+
+        '<wscn:CreateScanJobRequest>\n' +
+        '<JobIdentifier>Scan Identifier</JobIdentifier>\n' +
+        '<DestinationToken>Scan Dest Token</DestinationToken>\n' +
+        '<ScanTicket>\n' +
+        '<JobDescription>\n' +
+        '<JobName>Scan job1</JobName>\n' +
+        '<JobOriginationUserName>Scan user1</JobOriginationUserName>\n' +
+        '<JobInformation>Scan job info1</JobInformation>\n' +
+        '</JobDescription>\n' +
+        '<DocumentParameters>\n' +
+        '<Format>jfif</Format>\n' +
+        '<CompressionQualityFactor>80</CompressionQualityFactor>\n' +
+        '<ImagesToTransfer>0</ImagesToTransfer>\n' +
+        '<InputSource>Platen</InputSource>\n' +
+        '<ContentType>Auto</ContentType>\n' +
+        '<InputSize>\n' +
+        '<InputMediaSize>\n' +
+        '<Width Override="true">8500</Width>\n' +
+        '<Height Override="true">11690</Height>\n' +
+        '</InputMediaSize>\n' +
+        '<DocumentSizeAutoDetect>false</DocumentSizeAutoDetect>\n' +
+        '</InputSize>\n' +
+        '<Exposure>\n' +
+        '<AutoExposure>true</AutoExposure>\n' +
+        '<ExposureSettings>true</ExposureSettings>\n' +
+        '</Exposure>\n' +
+        '<MediaSides>\n' +
+        '<MediaFront>\n' +
+        '<ScanRegion>\n' +
+        '<ScanRegionXOffset UsedDefault="true">0</ScanRegionXOffset>\n' +
+        '<ScanRegionYOffset UsedDefault="true">0</ScanRegionYOffset>\n' +
+        '<ScanRegionWidth UsedDefault="true">8267</ScanRegionWidth>\n' +
+        '<ScanRegionHeight UsedDefault="true">11690</ScanRegionHeight>\n' +
+        '</ScanRegion>\n' +
+        '<ColorProcessing UsedDefault="true">RGB24</ColorProcessing>\n' +
+        '<Resolution>\n' +
+        '<Width>300</Width>\n' +
+        '<Height>300</Height>\n' +
+        '</Resolution>\n' +
+        '</MediaFront>\n' +
+        '</MediaSides>\n' +
+        '</DocumentParameters>\n' +
+        '<RetrieveImageTimeout>0</RetrieveImageTimeout>\n' +
+        '<ScanManufacturingParameters><DisableImageProcessing>false</DisableImageProcessing></ScanManufacturingParameters>\n' +
+        '</ScanTicket>\n' +
         '</wscn:CreateScanJobRequest >';
-    var func = _wrap_wscn_func(this.uri, xml);
+    var func = _wrap_wscn_immediate_func(uri, xml);
     return func(cb);
+};
+
+function _retrieve_image_request(uri, jobId, jobToken, write_stream, cb) {
+    var xml =
+        '<wscn:RetrieveImageRequest>\n' +
+        '<wscn:JobId>' + jobId + '</wscn:JobId>\n' +
+        '<wscn:JobToken>' + jobToken + '</wscn:JobToken>\n' +
+        '</wscn:RetrieveImageRequest>';
+    var func = _wrap_wscn_streaming_func(uri, xml, write_stream);
+    return func(cb);
+}
+
+ScannerSoap.prototype.GetScannerStatus = function GetScannerStatus(cb) {
+    _get_scanner_elements(this.uri, function (err, result) {
+        if (err) return cb(err);
+        return cb(undefined, result["wscn:ScanElements"]["ScannerStatus"]);
+    });
+};
+
+ScannerSoap.prototype.CreateJob = function CreateJob(cb) {
+    _create_scan_job_request(this.uri, function (err, job) {
+        if (err) return cb(err);
+        return cb(undefined, job["wscn:CreateScanJobResponseType"]);
+    });
+};
+
+ScannerSoap.prototype.RetrieveImage = function RetrieveImage(jobId, jobToken, write_stream, cb) {
+    _retrieve_image_request(this.uri, jobId, jobToken, write_stream, function (err) {
+        if (err) return cb(err);
+        return cb(undefined);
+    });
 };
 
 module.exports = ScannerSoap;
--- demo/scanner/test.js
+++ demo/scanner/test.js
@@ -1,22 +1,27 @@
 var ScannerSoap = require("./soap");
-var async = require("async");
+var fs = require("fs");
 
 var uri = 'http://192.168.40.37:8289/';
 var client = new ScannerSoap(uri);
 
-var jobs = [];
-jobs.push(function(cb){
-    client.GetScannerElements(function(err, json){
-        console.log("GetScannerElements", err, json);
-        cb();
-    });
+client.GetScannerStatus(function(err, status){
+    if(err) throw err;
+    var state = status["ScannerState"];
+    if(state === "Idle"){
+
+        client.CreateJob(function(err, job) {
+            if(err) throw err;
+            var jobId = job.JobId;
+            var jobToken = job.JobToken;
+            var writeStream = fs.createWriteStream("./test.jpg");
+
+            client.RetrieveImage(jobId, jobToken, writeStream, function(err){
+                console.log(err);
+            });
+
+        });
+
+    } else {
+        throw new Error(state)
+    }
 });
-
-//jobs.push(function(cb){
-//    client.CreateScanJobRequest(function(err, json){
-//        console.log("CreateScanJobRequest", err, json);
-//        cb();
-//    });
-//});
-
-async.series(jobs, console.log);
